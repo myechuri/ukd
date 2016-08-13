22a1ce449948da20f35245c47f45b560cb2cb546
Madhuri Yechuri <cosmokramer@gmail.com>
2016-08-12 17:30:42 -0700

    checkpoint

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+        "os/exec"
 	"github.com/myechuri/ukd/server/api"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/grpclog"
@@ -24,6 +25,28 @@ func (s ukdServer) GetVersion(context context.Context, request *api.VersionReque
 }
 
 func (s ukdServer) StartUK(context context.Context, request *api.StartRequest) (*api.StartReply, error) {
+        grpclog.Printf("StartUK request: name: %s, Image: %s", request.Name, request.Location)
+        cmdName := "qemu-system-x86_64"
+        args := []string{
+        "-m", "2G",
+        "-smp", "4",
+        "-vnc", ":1",
+        "-gdb", "tcp::1234,server,nowait",
+        "-device", "virtio-blk-pci,id=blk0,bootindex=0,drive=hd0,scsi=off",
+        "-drive", "file=/root/osv/build/debug/usr.img,if=none,id=hd0,cache=none,aio=native",
+        "-netdev", "tap,id=hn0,script=scripts/qemu-ifup.sh,vhost=on",
+        "-device", "virtio-net-pci,netdev=hn0,id=nic0",
+        "-redir", "tcp:2222::22",
+        "-device", "virtio-rng-pci",
+        "-enable-kvm",
+        "-cpu", "host,+x2apic",
+        "-chardev", "stdio,mux=on,id=stdio,signal=off",
+        "-mon", "chardev=stdio,mode=readline,default",
+        "-device", "isa-serial,chardev=stdio", }
+        cmd := exec.Command(cmdName, args...)
+        if err := cmd.Start(); err != nil {
+            grpclog.Fatalf(err)
+        }
 	reply := api.StartReply{
 		Success: false,
 		Ip:      "0.0.0.0",
@@ -33,6 +56,7 @@ func (s ukdServer) StartUK(context context.Context, request *api.StartRequest) (
 }
 
 func (s ukdServer) StopUK(context context.Context, request *api.StopRequest) (*api.StopReply, error) {
+        grpclog.Printf("StopUK request: name: %s", request.Name)
 	reply := api.StopReply{
 		Success: false,
 		Reason:  "Not yet implemented"}
